e0226e0014e717d2212e52499ab6f763d027b461
César Morgan <xds_xps_ivx@hotmail.com>
2020-02-25 13:03:13 -0600

    Nuevas impl. de FizzBuzz

---
+++ src/PowerShell/FizzBuzz.ps1
@@ -0,0 +1,45 @@
+﻿function Mark1 {
+    for ($j = 1; $j -le 100; $j++) {
+        $output = ""
+        if ($j % 3 -eq 0) {
+            $output += "Fizz"        
+        }
+        if ($j % 5 -eq 0) {
+            $output += "Buzz"
+        }
+        if ($output.Length -eq 0) {
+            $output = $j.ToString()
+        }
+        Write-Output $output
+    }
+}
+
+Function Mark2 {
+    $words = [System.Collections.Generic.Dictionary[System.Int32, System.String]]::new()        
+    
+    # Inicialización de palabras
+    $words.Add(3, "Fizz")
+    $words.Add(5, "Buzz")
+    
+    for ($j = 1; $j -le 100; $j++) 
+    {
+        $sb = ""
+
+        $en = $words.GetEnumerator()
+        while ($en.MoveNext()) {
+            if ($j % $en.Current.Key -eq 0)
+            {
+                $sb = [System.String]::Concat($sb, $en.Current.Value)
+            }
+        }
+        $en.Dispose()
+        if ($sb.Length -eq 0)
+        {
+            $sb = $j.ToString()
+        }
+        Write-Output $sb
+    }
+}
+
+Mark1
+Mark2
